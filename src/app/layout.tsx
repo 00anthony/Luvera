34f6cb3d649fd7e09d313c930d90e625b7760d0f
00anthony <anthonytij3@gmail.com>
2026-04-14 22:02:16 -0500

    feat: update metadata in layout for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://useluvera.com"),
+
   title: "Luvéra - Premium Men's Skincare | Lightweight Face Moisturizer",
   description: "Discover Luvéra's premium men's skincare. Lightweight, non-greasy face moisturizers for daily hydration across all skin types. Discover and shop today now online.",
+  keywords: [
+    "Skincare",
+    "lotion",
+    "moisturizer",
+    "men's",
+    "hydrating",
+  ],
   icons: {
     icon: "/favicon.png", // 👈 points to your new PNG
+  },
+
+  openGraph: {
+    title: "Luvéra - Premium Men's Skincare | Lightweight Face Moisturizer",
+    description:
+      "Discover Luvéra's premium men's skincare. Lightweight, non-greasy face moisturizers for daily hydration across all skin types. Discover and shop today now online.",
+    images: ['/product/tub.PNG'],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: "Luvéra - Premium Men's Skincare | Lightweight Face Moisturizer",
+    description:
+      "Discover Luvéra's premium men's skincare. Lightweight, non-greasy face moisturizers for daily hydration across all skin types. Discover and shop today now online.",
+    images: ['/product/tub.PNG'],
   },
 };
 
